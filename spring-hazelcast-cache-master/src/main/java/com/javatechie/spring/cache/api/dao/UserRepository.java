61a45391c8f12d7f5c36532cc8472d3c9b7b2745
jahanzebakhter <60420223+jahanzebakhter@users.noreply.github.com>
2023-09-09 21:12:08 -0700

    Delete spring-hazelcast-cache-master/src directory

--- spring-hazelcast-cache-master/src/main/java/com/javatechie/spring/cache/api/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javatechie.spring.cache.api.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.javatechie.spring.cache.api.model.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
