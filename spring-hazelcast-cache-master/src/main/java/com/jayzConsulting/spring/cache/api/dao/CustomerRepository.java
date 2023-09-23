ed8ac461e9b773a4d2bdf1f4fa14f914c2d77eb7
jahanzebakhter <60420223+jahanzebakhter@users.noreply.github.com>
2023-09-23 10:34:00 -0700

    Delete spring-hazelcast-cache-master/src/main directory

--- spring-hazelcast-cache-master/src/main/java/com/jayzConsulting/spring/cache/api/dao/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jayzConsulting.spring.cache.api.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jayzConsulting.spring.cache.api.model.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-
-}
--- spring-hazelcast-cache-master/src/main/java/com/jayzConsulting/spring/cache/api/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jayzConsulting.spring.cache.api.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jayzConsulting.spring.cache.api.model.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
